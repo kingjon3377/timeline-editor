8070e05d4d868f52b42f407ec370ae933ae32aaf
Jonathan Lovelace <kingjon3377@gmail.com>
2012-05-11 20:30:35 -0400

    Add 'previous event' field.

--- src/model/event/IEvent.java
+++ src/model/event/IEvent.java
@@ -18,4 +18,15 @@ public interface IEvent {
 	 * @return the date this event starts.
 	 */
 	int getDate();
+	
+	/**
+	 * @return the previous event. May not be null. If it's reference-equal to
+	 *         the object this method was called on, we've reached the start of
+	 *         the timeline.
+	 */
+	IEvent getPreviousEvent();
+	/**
+	 * @param event the new previous event
+	 */
+	void setPreviousEvent(final IEvent event);
 }
